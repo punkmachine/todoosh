f09637204afff21445af5c3617b9da31ec5afe91
PunkMashine <76869388+PunkMachinLearning@users.noreply.github.com>
2021-07-17 18:27:11 +0600

    Добавил форму входа.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,63 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-	//Modal
-	const modal = document.querySelector('.modal'),
-		modalClose = modal.querySelector('.modal__close'),
-		modalOpen = document.querySelector('.header__register');
-
-	modalOpen.addEventListener('click', (event) => {
-		event.preventDefault();
-
+	function modalOpen(modal) {
 		modal.classList.remove('modal_hide');
 		modal.classList.add('modal_show');
-	});
+	}
 
-	modalClose.addEventListener('click', (event) => {
-		event.preventDefault();
-
+	function modalClose(modal) {
 		modal.classList.add('modal_hide');
 		modal.classList.remove('modal_show');
+	}
+
+	//Modal Register
+	const modalRegister = document.querySelector('#register'),
+		modalRegisterClose = modalRegister.querySelector('.modal__close'),
+		modalRegisterOpen = document.querySelector('.header__register');
+
+	modalRegisterOpen.addEventListener('click', (event) => {
+		modalOpen(modalRegister);
+	});
+
+	modalRegisterClose.addEventListener('click', (event) => {
+		modalClose(modalRegister);
 	});
 
 	document.addEventListener('keydown', function(event) {
-		if (event.code === 'Escape' && modal.classList.contains('modal_show')) {
-			modal.classList.add('modal_hide');
-			modal.classList.remove('modal_show');
+		if (event.code === 'Escape' && modalRegister.classList.contains('modal_show')) {
+			modalClose(modalRegister);
 		}
 	});
 
-	modal.addEventListener('click', function(event) {
-		if (event.target === modal) {
-			modal.classList.add('modal_hide');
-			modal.classList.remove('modal_show');
+	modalRegister.addEventListener('click', function(event) {
+		if (event.target === modalRegister) {
+			modalClose(modalRegister);
+		}
+	});
+
+	// Modal Login
+	const modalLogin = document.querySelector('#login'),
+		modalLoginClose = modalLogin.querySelector('.modal__close'),
+		modalLoginOpen = document.querySelector('.header__login');
+
+	modalLoginOpen.addEventListener('click', (event) => {
+		modalOpen(modalLogin);
+	});
+
+	modalLoginClose.addEventListener('click', (event) => {
+		modalClose(modalLogin);
+	});
+
+	document.addEventListener('keydown', function(event) {
+		if (event.code === 'Escape' && modalLogin.classList.contains('modal_show')) {
+			modalClose(modalLogin);
+		}
+	});
+
+	modalLogin.addEventListener('click', function(event) {
+		if (event.target === modalLogin) {
+			modalClose(modalLogin);
 		}
 	});
 });
