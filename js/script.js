b472be416398fbbf003bd695e9b7a109b9c1db0b
PunkMashine <76869388+PunkMachinLearning@users.noreply.github.com>
2021-08-23 13:06:54 +0600

    Сделал получение тасков.

--- js/script.js
+++ js/script.js
@@ -56,6 +56,34 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
+	//получени всех тасков
+	let arrayTasks = [];
+
+	async function getData(url) {
+		const myHeaders = new Headers();
+
+		myHeaders.append('Content-Type', 'application/json');
+		myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyMzQxMiJ9.95ohW9ypI-87m3P6H-otIpPM-5W2iqeTucSWIdst8OU');
+
+		const res = await fetch(url, {
+			method: 'GET',
+			headers: myHeaders,
+		});
+
+		if (!res.ok) {
+			throw new Error(`Не получается обработать fetch ${url}, статус: ${res.status}`);
+		}
+
+		return res.json();
+	}
+
+	getData('http://localhost:8080/api/tasks')
+		.then((res) => {
+			arrayTasks = res;
+		}).catch((error) => {
+			console.log(error);
+		});
+
 	//modal add 
 	const modalAdd = document.querySelector('#add'),
 		  modalAddClose = modalAdd.querySelector('.modal__close'),
