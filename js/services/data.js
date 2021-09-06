6ec6ab253d9e1f6d23256b4e3fa93b0ac3889ad5
punkmachine <alex_rassydihin@mail.ru>
2021-09-06 14:34:13 +0600

    Добавляет функцию получения данных с сервера.
    Данную функцию использую еще и как функцию для метода delete.

--- js/services/data.js
+++ js/services/data.js
@@ -18,4 +18,21 @@ async function postData(url, json, method) {
 	return await res;
 }
 
-export { postData };
+async function getData(url, method) {
+	const myHeaders = {
+		'Content-Type': 'application/json;charset=utf-8',
+		'alg': 'HS256',
+		'typ': 'JWT',
+		'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzNDEyfQ.FGIdlz8lSwIByLlbX2K9Qp5xgZTtLuhD3YlH5yLq9NA'
+	}
+
+	const res = await fetch(url, {
+		method: method,
+		headers: myHeaders,
+		mode: 'cors'
+	});
+
+	return await res;
+}
+
+export { postData, getData };
