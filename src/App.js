8655b0c0495ef2ef3ab44294597ce4b973fbca90
punkmachine <alex_rassydihin@mail.ru>
2021-10-26 13:02:31 +0600

    Добавляет и подключает стилизованный футер.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.scss';
 
 import { Header } from './components/layout/header';
+import { Footer } from './components/layout/footer';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Footer />
 		</>
 	);
 }
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.scss';
+
+function Footer() {
+	return (
+		<footer className={styles.footer}>
+			<span className={styles.copyright}>(c) сделано Саней под наркотрипами.</span>
+		</footer>
+	);
+}
+
+export { Footer };
