7628daede7cb9358fa87b11481750e5750739b82
punkmachine <alex_rassydihin@mail.ru>
2021-10-27 17:45:59 +0600

    Добавляет компонент формы регистрации, уже с redux.

--- src/components/user/formReg.jsx
+++ src/components/user/formReg.jsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { registration } from '../../redux/action';
+
 import styles from './../../scss/modules/components/form.module.scss';
 
 function FormReg() {
+	const dispatch = useDispatch();
+	const data = useSelector(state => state.registrationReducer);
+	const [login, setLogin] = useState('');
+	const [pass, setPass] = useState('');
+	const [secondPass, setSecondPass] = useState('');
+
+	console.log('data >>> ', data);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+
+		dispatch(registration(login, pass, secondPass));
+	}
+
+	function handleLogin(event) {
+		setLogin(event.target.value);
+	}
+
+	function handlePass(event) {
+		setPass(event.target.value);
+	}
+
+	function handleSecondPass(event) {
+		setSecondPass(event.target.value);
+	}
+
 	return (
-		<form className={styles.form}>
-			<input className={styles.input} type="text" placeholder='Ваш логин' />
-			<input className={styles.input} type="password" placeholder='Ваш пароль' />
-			<input className={styles.input} type="password" placeholder='Повторите пароль' />
+		<form className={styles.form} onSubmit={handleSubmit}>
+			<input onChange={handleLogin} className={styles.input} type="text" placeholder='Ваш логин' />
+			<input onChange={handlePass} className={styles.input} type="password" placeholder='Ваш пароль' />
+			<input onChange={handleSecondPass} className={styles.input} type="password" placeholder='Повторите пароль' />
 			<button className={styles.btn}>Зарегестрироваться</button>
 		</form>
 	);
